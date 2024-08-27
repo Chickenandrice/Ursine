da1cfc4aeb437e47be31d9b5b6a59c6c5fe22d3b
Joey Chen <138754697+Chickenandrice@users.noreply.github.com>
2024-08-26 22:25:37 -0400

    Delete frontend/main.js

--- frontend/main.js
+++
@@ -1,7 +0,0 @@
-import './styles.css'
-import { hideHeader } from './hideHeader.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('main.js is connected!');
-  hideHeader();
-});
